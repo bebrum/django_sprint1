5132d9dabdf0b78c90c584aee8c0c393096c81d7
Vladislav Petrov <vladish.mail.ru@gmail.com>
2026-03-04 13:01:25 +1000

    PEP8 edits

--- blogicum/settings.py
+++ blogicum/settings.py
@@ -12,16 +12,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-53dl2f)-!0nb6_m44qt)&q-gkmzfqqh3&v&g!a2uh0pi=@j*32'
-
 
 DEBUG = True
 
